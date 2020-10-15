43b6ee8c172ddc0bd9d188373c4474e990edb058
Bartosz Gajda <bartek.g55@gmail.com>
2020-10-15 19:22:47 +0200

    Lists Intro.

---
+++ src/main/scala/practice/lists/ListProblems.scala
@@ -0,0 +1,43 @@
+package practice.lists
+
+import scala.annotation.tailrec
+
+sealed abstract class RList[+T] {
+  def head: T
+  def tail: RList[T]
+  def isEmpty: Boolean
+
+  def ::[S >: T](elem: S): RList[S] = new Cons[S](elem, this)
+}
+
+case object RNil extends RList[Nothing] {
+  override def head: Nothing = throw new NoSuchElementException
+  override def tail: RList[Nothing] = throw new NoSuchElementException
+  override def isEmpty: Boolean = true
+
+  override def toString: String = "[]"
+}
+
+case class ::[+T](override val head: T, override val tail: RList[T]) extends RList[T] {
+  override def isEmpty: Boolean = false
+
+  override def toString: String = {
+    @tailrec
+    def toStringTailrec(remaining: RList[T], result: String): String = {
+      if (remaining.isEmpty) result
+      else if (remaining.tail.isEmpty) s"$result${remaining.head}"
+      else toStringTailrec(remaining.tail, s"$result${remaining.head}, ")
+    }
+
+    "[" + toStringTailrec(this, "") + "]"
+  }
+}
+
+object ListProblems {
+  def main(args: Array[String]): Unit = {
+    RNil.::(2) == 2 :: RNil
+
+    val aSmallList = 1 :: 2 :: 3 :: RNil
+    println(aSmallList)
+  }
+}
